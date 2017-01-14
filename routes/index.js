3ce26b5ae28e03c566ce278313fef2ca5f7cf0ed
Gmfholley <gmfholley@gmail.com>
2017-01-14 15:33:29 -0600

    Rewrites default text a little

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-	title: "Wendy's silly site",
+	title: "Decision Maker",
+	subtitle: "What should I do?",
 	rankRange: 5,
 	numCriteria: 4,
 	numColors: 5,
 	choices: ["Choice 1", "Choice 2", "Choice 3"],
-	defaultCriteria: ["Cost", "Resources", "Customer Pain", "Urgency", "Buy-In", "Effect on Other Systems", "Difficulty", "Time", "Root Causes Addressed", "Extent Resolved", "Return on Investment", "Safety", "Training", "Team Control", "Cost to Maintain"]
+	criteria: ["Cost", "Resources", "Customer Pain", "Urgency", "Buy-In", "Effect on Other Systems", "Difficulty", "Time", "Root Causes Addressed", "Extent Resolved", "Return on Investment", "Safety", "Training", "Team Control", "Cost to Maintain"]
 	});
 });
 
